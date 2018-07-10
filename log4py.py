d33102d5bdddf367deda085cd760eea98c897ba9
liyatao <liyatao@csweep.com>
2018-07-10 18:36:11 +0800

    update

--- log4py.py
+++ log4py.py
@@ -7,49 +7,45 @@ mylog module
 本模块默认自动继承开关默认关闭，不会存在冲突问题
 """
 __author__ = "liyatao"
-__version__ = "1.0.4"
-__all__ = ["create_logger"]
+__version__ = "1.0.6"
+__all__ = ["create_logger", "FileHandler", "SMTPHandler"]
 
 import logging
 import os
-# from logging import FileHandler
 from logging.handlers import RotatingFileHandler
+from logging.handlers import SMTPHandler
+from logging import StreamHandler
 
-level = "info"
 _levels = {
     "debug": logging.DEBUG,
     "info": logging.INFO,
-    "warn": logging.WARN
+    "warn": logging.WARN,
+    "error": logging.ERROR
 }
+
 _format = {
     "module": ('%(asctime)s %(levelname)s %(name)s(%(funcName)s): %(message)s', '%Y-%m-%d %H:%M:%S'),
     "class": ('%(asctime)s %(levelname)s %(name)s: %(message)s', '%Y-%m-%d %H:%M:%S')
 }
 
-# class MyHandler(FileHandler):
 
-#     @property
-#     def baseFilename(self):
-#         self._baseFilename = '{}-{}'.format(self._baseFilename, datetime.datetime.now().strftime("%Y%m%d%S"))
-#         return self._baseFilename
-
-#     @baseFilename.setter
-#     def baseFilename(self, value):
-#         self._baseFilename = value
-#         return self._baseFilename
-
-
-class MyHandler(RotatingFileHandler):
+class FileHandler(RotatingFileHandler):
     _handlers = {}
 
-    def __new__(cls, filename, *args, **kw):
+    def __new__(cls, filename="main.log", *args, **kw):
+        # print(filename)
         keyname = os.path.normpath(filename)
         cls._handlers[keyname] = cls._handlers.get(keyname, object.__new__(cls))
         return cls._handlers[keyname]
 
+    def __init__(self, filename="main.log", maxBytes=20*1024*1024, backupCount=5):
+        super().__init__(filename, maxBytes, backupCount)
 
-def _add_handler(ltype, filename, maxBytes, backupCount, fmt):
-    handler = logging.StreamHandler() if filename is None else MyHandler(filename, maxBytes=maxBytes*1024*1024, backupCount=backupCount)
+
+def _add_handler(ltype, fmt, handler):
+    # handler = logging.StreamHandler() if filename is None else MyFileHandler(filename, maxBytes=maxBytes*1024*1024, backupCount=backupCount)
+    if handler is None:
+        handler = StreamHandler()
     if fmt is None:
         formatter = logging.Formatter(*_format[ltype])
     else:
@@ -58,37 +54,49 @@ def _add_handler(ltype, filename, maxBytes, backupCount, fmt):
     return handler
 
 
-def _gen_logger(name, level, ltype, filename, maxBytes, backupCount, fmt):
+def _gen_logger(name, level, ltype, fmt, handler):
     logger = logging.getLogger(name)
     if not logger.handlers:
-        logger.addHandler(_add_handler(ltype, filename, maxBytes, backupCount, fmt))
+        logger.addHandler(_add_handler(ltype, fmt, handler))
         logger.setLevel(_levels[level])
         logger.propagate = False
     return logger
 
 
-def bind_logger_to_object(*, level=level, filename=None, maxMBytes=20, backupCount=5, fmt=None, attr="logger"):
+def bind_logger_to_object(*, level="info", fmt=None, attr="logger", handler=None):
+    """类装饰器，绑定logger"""
     def decorator(instance):
         if instance.__module__ == "__main__":
             name = "%s(%s)" % ("main", instance.__name__)
         else:
             name = "%s(%s)" % (instance.__module__, instance.__name__)
-        logger = _gen_logger(name, level, "class", filename, maxMBytes, backupCount, fmt)
+        logger = _gen_logger(name, level, "class", fmt, handler)
         setattr(instance, attr, logger)
         return instance
     return decorator
 
 
-def create_logger_by_name(name, *, level=level, filename=None, maxMBytes=20, backupCount=5, fmt=None):
+def create_logger_by_name(name, *, level="info", fmt=None, handler=None):
+    """创建logger对象"""
     if name == "__main__":
         name = "main"
-    return _gen_logger(name, level, "module", filename, maxMBytes, backupCount, fmt)
+    return _gen_logger(name, level, "module", fmt, handler)
 
 
 def create_logger(*args, **kw):
     """
-    create_logger(attr="logger", level="info", fmt=None, filename=None, maxMBytes=20, backup=5) -> decorator to bind logger
-    create_logger(name, level="info", fmt=None, filename=None, maxMBytes=20, backup=5) -> a logger instance
+    create_logger(attr="logger", level="info", fmt=None, handler=None, config=None) -> decorator to bind logger
+    create_logger(name, level="info", fmt=None, handler=None, config=None) -> a logger instance
+    filehandler = FileHandler()
+    filelog = create_logger(__name__, handler=filehandler, level="warn")
+
+    @create_logger(attr="log")
+    class A:
+        pass
+
+    mailhandler = SMTPHandler(("smtp.163.com", 25), '180********@163.com', ['****@outlook.com'], "服务器异常告警", credentials=('180********', '*******'))
+    maillog = create_logger(__name__, handler=mailhandler)
+    maillog.info("hi,", exc_info=True)
     """
     if args:
         if isinstance(args[0], str):
--- test.py
+++ test.py
@@ -1,4 +1,4 @@
-from log4py import create_logger
+from log4py import create_logger, SMTPHandler
 import logging
 ch = logging.StreamHandler()
 logger = logging.getLogger()
@@ -7,11 +7,19 @@ logger.addHandler(ch)
 logger.info("root log")
 
 
-log = create_logger(__name__)
+params = {
+    "mailhost": ("smtp.163.com", 25), "fromaddr": '***********@163.com',
+    "toaddrs": ['****@outlook.com'], "subject": "服务器异常告警",
+    "credentials": ('***********', '********')
+}
+# log = create_logger(__name__, handler=SMTPHandler(**params), level="error")
+from log4py import FileHandler
+log = create_logger(__name__, handler=FileHandler("1.log"))
+
 log.info("hello world")
 
 def func():
-    log.info("hello function")
+    log.error("hello function")
 
 @create_logger(attr="log")
 class A:
